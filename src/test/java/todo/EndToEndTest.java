e0796d0e849c3ed01dc08a1e4a2ebeed5d425d62
Shorty <patrick.scheibert@hotmail.com>
2021-12-10 11:38:16 +0100

    try first test

---
+++ src/test/java/todo/EndToEndTest.java
@@ -0,0 +1,44 @@
+package todo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EndToEndTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    public void todo() {
+        String command = "Application";
+        String arg = "showTodos";
+
+        try {
+            Process myProcess = new ProcessBuilder(command, arg).start();
+            assertEquals("hello", outContent.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
